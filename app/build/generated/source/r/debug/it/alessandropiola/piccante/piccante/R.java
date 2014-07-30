d20a5da7b0e7efc2ee82bc4a27d776887182a543
alessandropiola <alessandro.gbridge@gmail.com>
2014-07-30 15:45:31 +0200

    Sistemazione LogIn GooglePlus

--- app/build/generated/source/r/debug/it/alessandropiola/piccante/piccante/R.java
+++ app/build/generated/source/r/debug/it/alessandropiola/piccante/piccante/R.java
@@ -214,7 +214,9 @@ containing a value of this type.
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
         public static final int sign_in_button=0x7f050005;
+        public static final int sign_out_button=0x7f050006;
         public static final int terrain=0x7f050003;
+        public static final int textView=0x7f050007;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f060000;
@@ -223,33 +225,34 @@ containing a value of this type.
         public static final int layout=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f070000;
-        public static final int auth_client_needs_enabling_title=0x7f070001;
-        public static final int auth_client_needs_installation_title=0x7f070002;
-        public static final int auth_client_needs_update_title=0x7f070003;
-        public static final int auth_client_play_services_err_notification_msg=0x7f070004;
-        public static final int auth_client_requested_by_msg=0x7f070005;
-        public static final int auth_client_using_bad_version_title=0x7f070006;
-        public static final int common_google_play_services_enable_button=0x7f070007;
-        public static final int common_google_play_services_enable_text=0x7f070008;
-        public static final int common_google_play_services_enable_title=0x7f070009;
-        public static final int common_google_play_services_install_button=0x7f07000a;
-        public static final int common_google_play_services_install_text_phone=0x7f07000b;
-        public static final int common_google_play_services_install_text_tablet=0x7f07000c;
-        public static final int common_google_play_services_install_title=0x7f07000d;
-        public static final int common_google_play_services_invalid_account_text=0x7f07000e;
-        public static final int common_google_play_services_invalid_account_title=0x7f07000f;
-        public static final int common_google_play_services_network_error_text=0x7f070010;
-        public static final int common_google_play_services_network_error_title=0x7f070011;
-        public static final int common_google_play_services_unknown_issue=0x7f070012;
-        public static final int common_google_play_services_unsupported_date_text=0x7f070013;
-        public static final int common_google_play_services_unsupported_text=0x7f070014;
-        public static final int common_google_play_services_unsupported_title=0x7f070015;
-        public static final int common_google_play_services_update_button=0x7f070016;
-        public static final int common_google_play_services_update_text=0x7f070017;
-        public static final int common_google_play_services_update_title=0x7f070018;
-        public static final int common_signin_button_text=0x7f070019;
-        public static final int common_signin_button_text_long=0x7f07001a;
+        public static final int TitoloMainPage=0x7f070000;
+        public static final int app_name=0x7f070001;
+        public static final int auth_client_needs_enabling_title=0x7f070002;
+        public static final int auth_client_needs_installation_title=0x7f070003;
+        public static final int auth_client_needs_update_title=0x7f070004;
+        public static final int auth_client_play_services_err_notification_msg=0x7f070005;
+        public static final int auth_client_requested_by_msg=0x7f070006;
+        public static final int auth_client_using_bad_version_title=0x7f070007;
+        public static final int common_google_play_services_enable_button=0x7f070008;
+        public static final int common_google_play_services_enable_text=0x7f070009;
+        public static final int common_google_play_services_enable_title=0x7f07000a;
+        public static final int common_google_play_services_install_button=0x7f07000b;
+        public static final int common_google_play_services_install_text_phone=0x7f07000c;
+        public static final int common_google_play_services_install_text_tablet=0x7f07000d;
+        public static final int common_google_play_services_install_title=0x7f07000e;
+        public static final int common_google_play_services_invalid_account_text=0x7f07000f;
+        public static final int common_google_play_services_invalid_account_title=0x7f070010;
+        public static final int common_google_play_services_network_error_text=0x7f070011;
+        public static final int common_google_play_services_network_error_title=0x7f070012;
+        public static final int common_google_play_services_unknown_issue=0x7f070013;
+        public static final int common_google_play_services_unsupported_date_text=0x7f070014;
+        public static final int common_google_play_services_unsupported_text=0x7f070015;
+        public static final int common_google_play_services_unsupported_title=0x7f070016;
+        public static final int common_google_play_services_update_button=0x7f070017;
+        public static final int common_google_play_services_update_text=0x7f070018;
+        public static final int common_google_play_services_update_title=0x7f070019;
+        public static final int common_signin_button_text=0x7f07001a;
+        public static final int common_signin_button_text_long=0x7f07001b;
     }
     public static final class style {
         /**  Customize your theme here. 
